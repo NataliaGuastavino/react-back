228349f33febf881d0251e76d6c307446179763e
NataliaGuastavino <a.nataliaguastavino@gmail.com>
2023-04-19 12:57:37 +0200

    Agrego el counter en la vista de detalle

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Card , Button} from 'react-bootstrap';
+import {ItemCount} from "../ItemCount/ItemCount"
 
-export const ItemDetail = ({id,name, price, image, category}) => {
+export const ItemDetail = ({name, price, image, category}) => {
     // Paso mediante props cada propiedad de los productos (objetos) y lo coloco por props en el retaurn
     return (
         <div className='item'>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '25rem' }}>
                 <Card.Img variant="top" src={image} />
                 <Card.Body>
                     <Card.Title className='nombre'>{name}</Card.Title>
                     <Card.Title className='precio'>{price}</Card.Title>
                     <Card.Title className='Categoría'>{category}</Card.Title>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi natus dolorum dolore. Voluptas voluptatem, obcaecati ducimus quia molestiae accusantium sapiente sint quasi at debitis libero quis numquam odit aliquid dignissimos.</p>
+                    <ItemCount/>
                     <Button className='btn-black'>Add to cart</Button>
                 </Card.Body>
             </Card>
